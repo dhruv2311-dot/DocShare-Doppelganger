fec3e26c816c604ea593ebad1efddafa5ee64dc0
shubhamiscodding <shubham.modi.cg@gmail.com>
2026-03-08 14:36:27 +0530

    allowing all the cors in the server.js

--- Docshare-Backend/server.js
+++ Docshare-Backend/server.js
@@ -17,8 +17,7 @@ connectDB();
 // Init Middleware
 app.use(express.json());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-  credentials: true,
+  origin: '*',
 }));
 
 // Define Routes
